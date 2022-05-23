7476787b80b47e22ef198a4330a444a4736581ba
Deepak yadav <deepaky541.dy@gmail.com>
2022-05-23 17:00:19 +0530

    update

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./tasks.module.css";
 import Task from "../Task/Task";
-
+import data from "../../data/tasks.json"
 
 const Tasks = (props) => {
   // NOTE: do not delete `data-cy` key value pair
@@ -13,6 +13,19 @@ const Tasks = (props) => {
       </ul>
       <div data-cy="tasks-empty" className={styles.empty}>
         {/* Show when No Tasks are present */}
+        {data.map((el) => (
+          <li data-cy="task" className={styles.task}>
+          <input
+            type="checkbox"
+            data-cy="task-checkbox"
+          />
+          <div data-cy="task-text" >{el.text}</div>
+          <button data-cy="task-counter-increment-button" >+</button>
+          <span data-cy="task-counter-value">{el.count}</span>
+          <button data-cy="task-counter-decrement-button" >-</button>
+          <button data-cy="task-remove-button">-</button>
+          </li>
+        ))}
       </div>
     </>
   );
